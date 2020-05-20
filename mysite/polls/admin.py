5e635efc3f13ec60b55c091784983ea84d661649
SeunghwanKang <rnltls95@naver.com>
2020-05-20 16:53:12 +0900

    [MERGE] take control with conflict

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-<<<<<<< HEAD
 from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
@@ -14,8 +13,3 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     
 admin.site.register(Question, QuestionAdmin)
-=======
-from .models import Question
-
-admin.site.register(Question)
->>>>>>> 7ceb300ffff9647f444a160801aae05cfa7e146d
