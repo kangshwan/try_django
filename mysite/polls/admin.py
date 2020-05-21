1914ce57c887782212e035b7436ea2cc49535f6e
SeunghwanKang <rnltls95@naver.com>
2020-05-21 20:01:25 +0900

    [UPDATE] part7 change title of admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -8,8 +8,13 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date','was_published_recently')
     list_filter = ['pub_date']
     inlines = [ChoiceInline]
+    search_fields = ['question_text']
     
 admin.site.register(Question, QuestionAdmin)
